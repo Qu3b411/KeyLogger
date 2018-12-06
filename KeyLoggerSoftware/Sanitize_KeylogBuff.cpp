b112b41b9ff73b76b37354a4961c907a39d6262c
Qu3b411 <19296119+Qu3b411@users.noreply.github.com>
2018-12-05 19:01:26 -0800

    messed up the update

--- KeyLoggerSoftware/Sanitize_KeylogBuff.cpp
+++ KeyLoggerSoftware/Sanitize_KeylogBuff.cpp
@@ -1,20 +1,46 @@
-#ifndef SANITIZE_KEYLOGBUFF_H_INCLUDED
-#define SANITIZE_KEYLOGBUFF_H_INCLUDED
-
-
-#define DefaultBufferSz     0x1000
-
-
-typedef struct XML_PROCESS_BUFFER
+#include "Sanitize_KeyLogBuff.h"
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+using namespace std;
+XML_PROCESS_BUFFER writeUnsanitizedBuffer(XML_PROCESS_BUFFER buffer, const char* str)
 {
-    unsigned int buffer_size;
-    unsigned int current_buff_offset;
-    char* buffered_data;
+    //    cout << strlen(str);
+        if(buffer.current_buff_offset+strlen(str) >= buffer.buffer_size)
+        {
+            buffer.buffer_size+=0x1000;
+            buffer.buffered_data = (char*)realloc(buffer.buffered_data,buffer.buffer_size);
+        }
+        memcpy(buffer.buffered_data+buffer.current_buff_offset,str,strlen(str));
+        buffer.current_buff_offset+=strlen(str);
+  //      cout<< (char)*(buffer.buffered_data+strlen(str)-2);
 
-}XML_PROCESS_BUFFER;
+    return buffer;
+}
 
-XML_PROCESS_BUFFER writeUnsanitizedBuffer(XML_PROCESS_BUFFER buf, const char* str);
-XML_PROCESS_BUFFER writeSanitizedBuffer(XML_PROCESS_BUFFER buf, const char* str);
-XML_PROCESS_BUFFER writeUnsanitizedByte(XML_PROCESS_BUFFER buff, const char ch);
-XML_PROCESS_BUFFER writeSanitizedByte(XML_PROCESS_BUFFER buff, const char ch);
-#endif // SANITIZE_KEYLOGBUFF_H_INCLUDED
+XML_PROCESS_BUFFER writeSanitizedByte(XML_PROCESS_BUFFER buffer, const char ch)
+{
+    switch(ch)
+    {
+        case '<':  buffer = writeUnsanitizedBuffer(buffer,"&lt;");break;
+        case '>':  buffer = writeUnsanitizedBuffer(buffer,"&gt;"); break;
+        case '&':  buffer = writeUnsanitizedBuffer(buffer,"&amp;"); break;
+        case '\'': buffer = writeUnsanitizedBuffer(buffer,"&apos;"); break;
+        case '"': buffer = writeUnsanitizedBuffer(buffer,"&quot;"); break;
+        default:  buffer = writeUnsanitizedByte(buffer,ch);
+
+    }
+    return buffer;
+}
+XML_PROCESS_BUFFER writeUnsanitizedByte(XML_PROCESS_BUFFER buffer, const char ch)
+{
+    *(buffer.buffered_data+buffer.current_buff_offset) = ch;
+    buffer.current_buff_offset++;
+    return buffer;
+}
+XML_PROCESS_BUFFER writeSanitizedBuffer(XML_PROCESS_BUFFER buffer, const char* str)
+{
+    for(int x = 0; x<strlen(str);x++)
+        buffer = writeSanitizedByte(buffer,*(str+x));
+   return buffer;
+}
