b309dc83b1b3dbd3cfd27a7199685cce0ecbb203
Qu3b411 <19296119+Qu3b411@users.noreply.github.com>
2018-12-30 21:27:39 -0800

    improve compiler script,
    still need to add javac and directory structure before I release the script

--- Compile.bat
+++ Compile.bat
@@ -23,7 +23,7 @@ if /I "%windowd%" EQU "N" goto :exit
 goto :Server
 :ServerCompiler
 echo %cd%
-mingw32-g++.exe  -O2 -std=c++11 -I"C:\Program Files\Java\jdk1.8.0_191\include" -I"C:\Program Files\Java\jdk1.8.0_191\include\win32" -I"C:\Program Files (x86)\Java\jdk1.8.0_191\include\win32" -I"C:\Program Files (x86)\Java\jdk1.8.0_191\include" -c C:\Users\Qu3b411\Desktop\KeyLogger-master_2\KeyLogger-master\KeyLoggerSoftware\server\main.cpp -o %cd%\KeyLoggerSoftware\server\keyloggerserver.o
+mingw32-g++.exe  -O2 -std=c++11 -I"C:\Program Files (x86)\Java\jdk1.8.0_191\include" -I"C:\Program Files\Java\jdk1.8.0_191\include\win32" -I"C:\Program Files (x86)\Java\jdk1.8.0_191\include\win32" -I"C:\Program Files (x86)\Java\jdk1.8.0_191\include" -c %cd%\KeyLoggerSoftware\server\main.cpp -o %cd%\KeyLoggerSoftware\server\keyloggerserver.o
 mingw32-g++.exe -L"C:\Program Files (x86)\Java\jdk1.8.0_191\jre\bin\server" -L"C:\Program Files (x86)\Java\jdk1.8.0_191\jre\bin\plugin2" -L"C:\Program Files (x86)\Java\jdk1.8.0_191\jre\bin\server" -L"C:\Program Files (x86)\Java\jre1.8.0_191\bin\client" -o %cd%\KeyLoggerSoftware\server\keyloggerServerGui.exe %cd%\KeyLoggerSoftware\server\keyloggerserver.o  -s -lws2_32 -std=c++11  "C:\Program Files (x86)\Java\jdk1.8.0_191\lib\jvm.lib" "C:\Program Files (x86)\Java\jdk1.8.0_191\lib\jawt.lib"
 
 
