a62dc6d00c13214266bbf81bae1ea544f870e55f
Qu3b411 <19296119+Qu3b411@users.noreply.github.com>
2019-01-02 13:01:47 -0800

    header used to set colors in console

---
+++ KeyLoggerSoftware/server/ConsoleColors.h
@@ -0,0 +1,9 @@
+#ifndef CONSOLECOLORS_H_INCLUDED
+#define CONSOLECOLORS_H_INCLUDED
+#define ClipBoardColor 0x30
+#define ProccessInfoColor 0x2f
+#define TitleInfoColor 0x4f
+#define keylogColor 0x60
+
+
+#endif // CONSOLECOLORS_H_INCLUDED
